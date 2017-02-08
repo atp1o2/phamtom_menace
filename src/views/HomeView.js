923b78a77e31328a85af615d53a133eda58a62ec
Andrew Pham <andrewpham8@gmail.com>
2017-02-07 16:02:50 -0800

    Clarity mock (#4)
    * Create masthead
    
    * Build a Clarity frontend mock

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class HomeView extends Component {
   render () {
-    return <h3>This is the Home View</h3>
+    return (
+      <div className="home mt-3">
+        <div className="container py-5">
+          <h1 className="text-primary">Next Generation</h1>
+          <h1 className="text-primary">Surgical Planning</h1>
+          <p className="py-3 h4">
+            Clarity is developing a cloud-based software application that combines advanced medical imaging technologies with data analytics to provide surgeons with a far better way of planning for their operations.
+          </p>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/views/MastheadView.js
+++ src/views/MastheadView.js
@@ -4,13 +4,27 @@ import { Link } from 'react-router';
 class MastheadView extends Component {
   render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Habitual</h2>
-          <button><Link to="/">Home</Link></button>
-          <button><Link to="/About">About</Link></button>
-          <button><Link to="/Task">Tasks</Link></button>
-
+      <div className="masthead">
+        <div className="container">
+          <div className="d-inline-flex">
+            <img className="brand-logo" src="https://static.wixstatic.com/media/234003_7e0b776028ae441bb22bdaff2cd0bcfd~mv2.jpg/v1/fill/w_120,h_120,al_c,q_80,usm_0.66_1.00_0.01/234003_7e0b776028ae441bb22bdaff2cd0bcfd~mv2.webp"/>
+          </div>
+          <div className="mt-5 d-inline-flex float-md-right float-sm-none">
+            <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link text-muted vertical-divider" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-muted vertical-divider" to="/Contact">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-muted vertical-divider" to="/Team">Team</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-muted vertical-divider" to="/Demo">Demo</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
